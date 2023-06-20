cef5576d69a7a6a23aea52940c92c8f7da03efc8
OhodAlii <135612243+OhodAlii@users.noreply.github.com>
2023-06-20 04:18:33 +0300

    Update testRecipeMore.py Add (test_edit_recipe)

--- testRecipeMore.py
+++ testRecipeMore.py
@@ -32,3 +32,17 @@ class TestRecipeManager(unittest.TestCase):
             self.manager.delete_recipe()  # Enter ID of non existent recipe
 
     @patch('builtins.input', side_effect=["", "", "", "", "", ""])
+    
+    def test_edit_recipe(self, mock_input):
+        # Trying to update a non existent recipe
+        print("")
+        print("Testing updating a Recipe")
+        print("")
+        test_recipe = Recipe(500, "kunafa", "cream honey cheese sugar syrup",
+                             "1) mix evrything and boom", "dessert", "3")
+        self.manager = RecipeManager()
+        self.manager.recipes.append(test_recipe)
+
+        with self.assertRaises(Exception):
+            self.manager.edit_recipe()  # Enter ID of non existent recipe
+
